46c36cc701ae998ac6db2628652ae71d1716b0be
Tushar <tushar27x@proton.me>
2026-08-23 11:33:45 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    mem = {}
+    def climbStairs(self, n: int) -> int:
+        if n in self.mem:
+            return self.mem[n]
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        res = self.climbStairs(n-1) + self.climbStairs(n-2)
+        self.mem[n] = res
+        return res
